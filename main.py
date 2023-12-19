16322e97e02e7ca56b8aec0418cae722824b897c
fajrCode <fajri42vandi@gmail.com>
2023-12-19 09:55:58 +0700

    learn variable, input, and output

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 print("hello World")
+
+greeting = "Hi Berlian"
+
+print(greeting+" apa kabar")
+
+addition = 1+3
+
+result = addition - 1
+
+print(result)
+
+name = input("Masukkan nama anda: ")
+
+print(name)
+
+#this is comment with python
